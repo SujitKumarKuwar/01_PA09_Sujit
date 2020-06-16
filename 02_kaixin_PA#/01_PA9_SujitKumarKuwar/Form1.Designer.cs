63111f97ad667f37af0b6ccd50aa1e1ca2c06011
kaixin02 <64114925+Prestigesz@users.noreply.github.com>
2020-06-16 12:51:18 +0800

    Updated by kaixin

---
+++ 02_kaixin_PA#/01_PA9_SujitKumarKuwar/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace _01_PA9_SujitKumarKuwar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.rdb_JapaneseYen = new System.Windows.Forms.RadioButton();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.lbl_amount = new System.Windows.Forms.Label();
+            this.rdb_USdollars = new System.Windows.Forms.RadioButton();
+            this.lbl_selectcurrency = new System.Windows.Forms.Label();
+            this.lbl_value = new System.Windows.Forms.Label();
+            this.txt_amount = new System.Windows.Forms.TextBox();
+            this.txt_convertedAmt = new System.Windows.Forms.TextBox();
+            this.rdb_MalaysianRinggit = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdb_MalaysianRinggit);
+            this.panel1.Controls.Add(this.btn_clear);
+            this.panel1.Controls.Add(this.rdb_JapaneseYen);
+            this.panel1.Controls.Add(this.btn_convert);
+            this.panel1.Controls.Add(this.lbl_amount);
+            this.panel1.Controls.Add(this.rdb_USdollars);
+            this.panel1.Controls.Add(this.lbl_selectcurrency);
+            this.panel1.Controls.Add(this.lbl_value);
+            this.panel1.Controls.Add(this.txt_amount);
+            this.panel1.Controls.Add(this.txt_convertedAmt);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(601, 369);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(430, 182);
+            this.btn_clear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 31);
+            this.btn_clear.TabIndex = 7;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // rdb_JapaneseYen
+            // 
+            this.rdb_JapaneseYen.AutoSize = true;
+            this.rdb_JapaneseYen.Location = new System.Drawing.Point(182, 162);
+            this.rdb_JapaneseYen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdb_JapaneseYen.Name = "rdb_JapaneseYen";
+            this.rdb_JapaneseYen.Size = new System.Drawing.Size(93, 17);
+            this.rdb_JapaneseYen.TabIndex = 9;
+            this.rdb_JapaneseYen.TabStop = true;
+            this.rdb_JapaneseYen.Text = "Japanese Yen";
+            this.rdb_JapaneseYen.UseVisualStyleBackColor = true;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(430, 122);
+            this.btn_convert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(75, 28);
+            this.btn_convert.TabIndex = 6;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // lbl_amount
+            // 
+            this.lbl_amount.AutoSize = true;
+            this.lbl_amount.Location = new System.Drawing.Point(86, 54);
+            this.lbl_amount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_amount.Name = "lbl_amount";
+            this.lbl_amount.Size = new System.Drawing.Size(43, 13);
+            this.lbl_amount.TabIndex = 1;
+            this.lbl_amount.Text = "Amount";
+            this.lbl_amount.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rdb_USdollars
+            // 
+            this.rdb_USdollars.AutoSize = true;
+            this.rdb_USdollars.Location = new System.Drawing.Point(182, 128);
+            this.rdb_USdollars.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdb_USdollars.Name = "rdb_USdollars";
+            this.rdb_USdollars.Size = new System.Drawing.Size(75, 17);
+            this.rdb_USdollars.TabIndex = 8;
+            this.rdb_USdollars.TabStop = true;
+            this.rdb_USdollars.Text = "US Dollars";
+            this.rdb_USdollars.UseVisualStyleBackColor = true;
+            this.rdb_USdollars.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // lbl_selectcurrency
+            // 
+            this.lbl_selectcurrency.AutoSize = true;
+            this.lbl_selectcurrency.Location = new System.Drawing.Point(86, 128);
+            this.lbl_selectcurrency.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_selectcurrency.Name = "lbl_selectcurrency";
+            this.lbl_selectcurrency.Size = new System.Drawing.Size(82, 13);
+            this.lbl_selectcurrency.TabIndex = 2;
+            this.lbl_selectcurrency.Text = "Select Currency";
+            // 
+            // lbl_value
+            // 
+            this.lbl_value.AutoSize = true;
+            this.lbl_value.Location = new System.Drawing.Point(86, 235);
+            this.lbl_value.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_value.Name = "lbl_value";
+            this.lbl_value.Size = new System.Drawing.Size(34, 13);
+            this.lbl_value.TabIndex = 3;
+            this.lbl_value.Text = "Value";
+            // 
+            // txt_amount
+            // 
+            this.txt_amount.Location = new System.Drawing.Point(152, 54);
+            this.txt_amount.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_amount.Name = "txt_amount";
+            this.txt_amount.Size = new System.Drawing.Size(238, 20);
+            this.txt_amount.TabIndex = 4;
+            this.txt_amount.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_convertedAmt
+            // 
+            this.txt_convertedAmt.Location = new System.Drawing.Point(152, 228);
+            this.txt_convertedAmt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_convertedAmt.Name = "txt_convertedAmt";
+            this.txt_convertedAmt.ReadOnly = true;
+            this.txt_convertedAmt.Size = new System.Drawing.Size(238, 20);
+            this.txt_convertedAmt.TabIndex = 5;
+            // 
+            // rdb_MalaysianRinggit
+            // 
+            this.rdb_MalaysianRinggit.AutoSize = true;
+            this.rdb_MalaysianRinggit.Location = new System.Drawing.Point(182, 189);
+            this.rdb_MalaysianRinggit.Margin = new System.Windows.Forms.Padding(2);
+            this.rdb_MalaysianRinggit.Name = "rdb_MalaysianRinggit";
+            this.rdb_MalaysianRinggit.Size = new System.Drawing.Size(108, 17);
+            this.rdb_MalaysianRinggit.TabIndex = 10;
+            this.rdb_MalaysianRinggit.TabStop = true;
+            this.rdb_MalaysianRinggit.Text = "Malaysian Ringgit";
+            this.rdb_MalaysianRinggit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_amount;
+        private System.Windows.Forms.Label lbl_selectcurrency;
+        private System.Windows.Forms.Label lbl_value;
+        private System.Windows.Forms.TextBox txt_amount;
+        private System.Windows.Forms.TextBox txt_convertedAmt;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.RadioButton rdb_USdollars;
+        private System.Windows.Forms.RadioButton rdb_JapaneseYen;
+        private System.Windows.Forms.RadioButton rdb_MalaysianRinggit;
+    }
+}
+
---
+++ 02_kaixin_PA#/01_PA9_SujitKumarKuwar/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_PA9_SujitKumarKuwar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            double AmountEntered;
+            double convertedvalue;
+            try
+            {
+                if ((rdb_USdollars.Checked == false) || (rdb_JapaneseYen.Checked = false))
+                {
+                    txt_convertedAmt.Text = "Select at least one type of currency to convert";
+                }
+
+                if (rdb_USdollars.Checked == true)
+                {
+                    AmountEntered = double.Parse(txt_amount.Text);
+                    convertedvalue = AmountEntered * 0.74;
+
+                    txt_convertedAmt.Text = convertedvalue.ToString();
+
+
+                }
+
+                if (rdb_JapaneseYen.Checked == true)
+                {
+                    AmountEntered = double.Parse(txt_amount.Text);
+                    convertedvalue = AmountEntered * 81.97;
+
+                    txt_convertedAmt.Text = convertedvalue.ToString();
+                }
+                else if (rdb_MalaysianRinggit.Checked == true)
+                {
+                    AmountEntered = double.Parse(txt_amount.Text);
+                    convertedvalue = AmountEntered * 3.01;
+
+                    txt_convertedAmt.Text = convertedvalue.ToString();
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid value.");
+                txt_amount.Text = "";
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_amount.Clear();
+            txt_convertedAmt.Clear();
+
+        }
+    }
+}
